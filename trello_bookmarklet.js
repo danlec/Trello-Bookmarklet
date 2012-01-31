c1d92d160ff7cf7571f11cbfd0a8ccbe7e8de2c1
Daniel LeCheminant <danlec@gmail.com>
2012-01-31 15:11:53 -0500

    initial commit

---
+++ trello_bookmarklet.js
@@ -0,0 +1,240 @@
+(function(window){
+  var $;
+
+  /* This is run after we've connected to Trello and selected a list */
+  var run = function(Trello, idList) {
+    var name;
+    // Default description is the URL of the page we're looking at
+    var desc = location.href;
+
+    if(window.goBug) {
+
+      // We're looking at a FogBugz case
+      name = goBug.ixBug + ": " + goBug.sTitle
+
+    } else if ($("#issue_header_summary").length){
+
+      // We're looking at a JIRA case
+      name = $("#key-val").text() + ": " + $("#issue_header_summary").text();
+
+    } else if ($("#show_issue").length) {
+
+      // We're looking at a GitHub issue
+      name = $("#show_issue .number strong").text() + " " + $("#show_issue .content-title").text();
+
+    } else {
+
+      // It isn't anything we recognize, but we'll see if we can make something using the selected text
+
+    }
+
+    // Get any selected text
+    var selection;
+
+    if(window.getSelection) {
+      selection = ""+window.getSelection();
+    } else if(document.selection && document.selection.createRange) {
+      selection = document.selection.createRange().text;
+    }
+
+    // If they've selected text, add it to the name/desc of the card
+    if(selection) {
+      if(!name) {
+        name = selection;
+      } else {
+        desc += "\n\n" + selection;
+      }
+    }
+
+    // Create the card
+    if(name) {
+      Trello.post("lists/" + idList + "/cards", { 
+        name: name, 
+        desc: desc
+      })
+    }
+  }
+
+  var storage = window.localStorage;
+  if(!storage) {
+    return;
+  }
+
+  // Store/retrieve a value from local storage
+  var store = function(key, value){
+    if(arguments.length == 2){
+      return (storage[key] = value);
+    } else {
+      return storage[key];
+    }
+  };
+
+  // A fake "prompt" to get info from the user
+  var overlayPrompt = function(html, hasInput, callback){
+    var done = function(value){
+      $div.remove();
+      $overlay.remove();
+      callback(value);
+    };
+
+    // Cover the existing webpage with an overlay
+    var $overlay = $("<div>")
+    .css({
+      background: "#000",
+      opacity: .75,
+      "z-index": 1e4,
+      position: "absolute",
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom: 0
+    })
+    .appendTo("body")
+    .click(function(){
+      done(null);
+    })
+
+    // Show a "popup"
+    var $div = $("<div>")
+    .css({
+      position: "absolute",
+      border: "1px solid #000",
+      padding: "16px",
+      width: 300,
+      top: 64,
+      left: ($(window).width() - 200) / 2,
+      background: "#fff",
+      "z-index": 1e5
+    })
+    .appendTo("body");
+
+    // Show the prompt
+    $("<div>").html(html).appendTo($div);
+
+    // Optionally show an input
+    var $input = $("<input>")
+    .css({ 
+      width: "100%",
+      "margin-top": "8px"
+    })
+    .appendTo($div)
+    .toggle(hasInput);
+
+    // Add an "OK" button
+    $("<div>")
+    .text("OK")
+    .css({ 
+      width: "100%", 
+      "text-align": "center",
+      border: "1px solid #000",
+      background: "#eee",
+      "margin-top": "8px"
+    })
+    .appendTo($div)
+    .click(function(){
+      done($input.val());      
+    });
+
+    return $div;
+  };
+
+  // Run several asyncronous functions in order
+  var waterfall = function(fxs){
+    var runNext = function(){
+      if(fxs.length){
+        fxs.shift().apply(null, Array.prototype.slice.call(arguments).concat([runNext]))
+      }
+    }
+    runNext();
+  }
+
+  // The ids of values we keep in localStorage
+  var appKeyName = "trelloAppKey";
+  var idListName = "trelloIdList";
+
+  waterfall([
+    // Load jQuery
+    function(next) {
+      if(window.jQuery) {
+        next(null);
+      } else {
+        var script = document.createElement("script");
+        script.src = "http://code.jquery.com/jquery-1.7.1.min.js";
+        script.onload = next;
+        document.getElementsByTagName("head")[0].appendChild(script);
+      }
+    },
+    // Get the user's App Key, either from local storage, or by prompting them to retrieve it
+    function(ev, next) {
+      $ = window.jQuery;
+
+      var appKey = store(appKeyName);
+      if(appKey) {
+        next(appKey);
+      }
+      else {
+        overlayPrompt("Please specify your Trello API Key (you'll only need to do this once per site)<br><br>You can get your API Key <a href='https://trello.com/1/appKey/generate' target='apikey'>here</a><br><br>", true, function(newAppKey){
+          if(newAppKey) {
+            next(newAppKey);
+          }
+        })
+      }
+    },
+    // Load the Trello script
+    function(appKey, next) { $.getScript("https://trello.com/1/client.js?key=" + appKey, next); },
+    // Authorize our application
+    function(a, b, c, next) {
+      store(appKeyName, Trello.key())
+      Trello.authorize({
+        interactive: false,
+        success: next,
+        error: function(){
+          overlayPrompt("You need to authorize Trello", false, function(){
+            Trello.authorize({
+              type: "popup",
+              expiration: "never",
+              scope: { read: true, write: true },
+              success: next
+            });
+          });
+        }
+      });
+    },
+    // Get the list to add cards to, either from local storage or by prompting the user
+    function(next) {
+      var idList = store(idListName);
+      if(idList) {
+        next(idList);
+      } else {
+        Trello.get("members/me/boards", { fields: "name" }, function(boards){
+          $prompt = overlayPrompt('Which list should cards be sent to?<hr><div class="boards"></div>', false, function(){
+            idList = $prompt.find("input:checked").attr("id");
+            next(idList);
+          })
+
+          $.each(boards, function(ix, board){
+            $board = $("<div>").appendTo($prompt.find(".boards"))
+
+            Trello.get("boards/" + board.id + "/lists", function(lists){
+              $.each(lists, function(ix, list) {
+                var $div = $("<div>").appendTo($board);
+                idList = list.id;
+                $("<input type='radio'>").attr("id", idList).attr("name", "idList").appendTo($div);
+                $("<label>").text(board.name + " : " + list.name).attr("for", idList).appendTo($div);
+              });
+            })
+          });
+        });
+      }      
+    },
+    // Store the idList for later
+    function(idList, next) {
+      if(idList) {
+        store(idListName, idList);
+        next(Trello, idList);
+      }      
+    },
+    // Run the user portion
+    run
+  ]);
+})(window);
